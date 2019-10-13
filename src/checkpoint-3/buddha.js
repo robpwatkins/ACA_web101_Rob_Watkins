cd09030502ae61641adebd979fcff291e700f8d4
Rob W <robpwatkins@gmail.com>
2019-10-12 21:55:53 -0500

    Still clicking

--- src/checkpoint-3/buddha.js
+++ src/checkpoint-3/buddha.js
@@ -7,23 +7,3 @@ $("#BuddhaBox").click(function() {
     count++;
     $(".Counter").html(+count);
 });
-
-
-// $("#.Buddha").click(function() {
-//     count += 120; // increase by 120
-//     $(".Counter").html(+count);
-// });
-
-
-
-
-
-
-
-
-
-
-
-// $('#BuddhaBox').on('click', function() {
-//   $('.Counter').css('background-color', 'red');
-// });
