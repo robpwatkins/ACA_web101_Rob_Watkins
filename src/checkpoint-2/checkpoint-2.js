bde45242622d7dca8a56f1b6302f87b93d5ea6fa
Rob W <robpwatkins@gmail.com>
2019-10-16 13:01:50 -0500

    Checkpoint 2 is like, perfect.

---
+++ src/checkpoint-2/checkpoint-2.js
@@ -0,0 +1,13 @@
+$('.fa-bars').click(function() {
+  $('ul').toggleClass('active');
+  $('.toggle').toggleClass('bars-active');
+  $('.toggleX').toggleClass('X-active');
+  $('body').toggleClass('ScrollLock');
+})
+
+$('.fa-times').click(function() {
+  $('ul').toggleClass('active');
+  $('.toggle').toggleClass('bars-active');
+  $('.toggleX').toggleClass('X-active');
+  $('body').toggleClass('ScrollLock');
+})
